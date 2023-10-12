1f32d22379f1bb991cfbf22192154fa76a604596
rockyTTTT <107424045+rockyTTTT@users.noreply.github.com>
2023-10-12 14:07:46 +0800

    项目初始化

---
+++ src/test/java/lab/zhang/servicedemo/ServiceDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package lab.zhang.servicedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
